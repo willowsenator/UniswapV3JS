d91459a44223eadd5dd895638f4667dd804eab1a
willowsenator <willowsenator@gmail.com>
2022-09-20 22:39:59 +0200

    Ethers Crash Course

--- src/Web3Ethers.js
+++ src/Web3Ethers.js
@@ -16,3 +16,22 @@
 // getWeb3Details()
 
 // ETHERS
+// import { ethers } from "ethers";
+//
+// const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/1680b86acdf84d6a99da734a57721b84");
+// const ABI = [
+//     "function name() view returns (string)",
+//     "function symbol() view returns (string)",
+//     "function decimals() view returns (uint256)"
+// ];
+// const address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07';
+// const omgContract = new ethers.Contract(address, ABI, provider);
+//
+// const getEthersDetails = async () => {
+//     let tokenName = await omgContract.name();
+//     let tokenDecimals = await omgContract.decimals();
+//     let tokenSymbol = await omgContract.symbol();
+//     console.log(tokenName, tokenDecimals.toString(), tokenSymbol);
+// }
+//
+// getEthersDetails()
