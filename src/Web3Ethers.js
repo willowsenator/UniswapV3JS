35f703fddb85298fe86b609dd08dc66b649e6de2
willowsenator <willowsenator@gmail.com>
2022-09-20 22:12:13 +0200

    Web3 crash course

---
+++ src/Web3Ethers.js
@@ -0,0 +1,18 @@
+// WEB3
+// import abi from './abi.json' assert {type: 'json'}
+// import Web3 from 'web3';
+// const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/1680b86acdf84d6a99da734a57721b84'));
+// const address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07';
+//
+// const TestContract = new web3.eth.Contract(abi, address);
+//
+// const getWeb3Details = async () => {
+//     let tokenName = await TestContract.methods.name().call();
+//     let tokenDecimals = await TestContract.methods.decimals().call();
+//     let tokenSymbol = await TestContract.methods.symbol().call();
+//     console.log(tokenName, tokenDecimals, tokenSymbol);
+// }
+//
+// getWeb3Details()
+
+// ETHERS
